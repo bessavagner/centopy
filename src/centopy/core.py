e73da992fbe1464b5c51689144294e9f81cd3c4e
bessavagner <bessavagner@gmail.com>
2023-08-21 15:51:32 -0300

    feat(core.Compressor): add zip files handler api

--- src/centopy/core.py
+++ src/centopy/core.py
@@ -5,6 +5,8 @@
 """
 
 import logging
+import zipfile
+from pathlib import Path
 from .base import BaseFilesManager
 
 logger = logging.getLogger('standard')
@@ -28,13 +30,13 @@ class FilesManager(BaseFilesManager):
     def __init__(self, folder_path: str, *args, **kwargs):
         super().__init__(folder_path, *args, **kwargs)
 
-    def write(
+    def _open_write(
             self,
-            file_name,
-            file_contents,
+            file_name: str,
+            file_contents: bytes,
+            mode='w',
             encoding="utf-8",
-            **kwargs
-    ):
+            **kwargs):
         """
         Save the contents to a file.
 
@@ -44,6 +46,8 @@ class FilesManager(BaseFilesManager):
             The name of the file to save.
         file_contents : str
             The contents to save to the file.
+        mode: str, optional
+            Specifies the mode in which the file is opened, by default 'w'.
         encoding : str, optional
             The encoding of the file, by default "utf-8".
         **kwargs
@@ -51,14 +55,14 @@ class FilesManager(BaseFilesManager):
 
         """
         with open(
-            self.file_path(file_name), 'w', encoding=encoding, **kwargs
+            self.file_path(file_name), mode, encoding=encoding, **kwargs
         ) as file_:
             file_.write(file_contents)
         state = self.file_state.setdefault(file_name, [])
         state.append("saved")
         self.file_state[file_name] = state
 
-    def read(self, file_name, mode='r', encoding="utf-8", **kwargs):
+    def _open_read(self, file_name, mode='r', encoding="utf-8", **kwargs):
         """
         Load the contents of a file.
 
@@ -90,3 +94,230 @@ class FilesManager(BaseFilesManager):
             logger.error("File not found: %s. Returning None", file_name)
         self.file_state[file_name] = state
         return file_contents
+        
+
+    def write(
+            self,
+            file_name: str,
+            file_contents: bytes,
+            encoding="utf-8",
+            **kwargs):
+        """
+        Save the contents to a file.
+
+        Parameters
+        ----------
+        file_name : str
+            The name of the file to save.
+        file_contents : str
+            The contents to save to the file.
+        encoding : str, optional
+            The encoding of the file, by default "utf-8".
+        **kwargs
+            Additional keyword arguments to pass to the open() function.
+
+        """
+        self._open_write(file_name, file_contents, 'w', encoding, **kwargs)
+
+    def writeb(
+            self,
+            file_name: str,
+            file_contents: bytes,
+            **kwargs
+    ):
+        """
+        Save the binary contents to a file.
+
+        Parameters
+        ----------
+        file_name : str
+            The name of the file to save.
+        file_contents : bytes
+            The contents to save to the file.
+        encoding : str, optional
+            The encoding of the file, by default "utf-8".
+        **kwargs
+            Additional keyword arguments to pass to the open() function.
+
+        """
+        self._open_write(
+            file_name, file_contents, 'wb', encoding=None, **kwargs
+        )
+
+    def append(
+            self,
+            file_name: str,
+            file_contents: bytes,
+            encoding="utf-8",
+            **kwargs
+    ):
+        """
+        Save the contents to a file.
+
+        Parameters
+        ----------
+        file_name : str
+            The name of the file to save.
+        file_contents : str
+            The contents to save to the file.
+        encoding : str, optional
+            The encoding of the file, by default "utf-8".
+        **kwargs
+            Additional keyword arguments to pass to the open() function.
+
+        """
+        self._open_write(file_name, file_contents, 'a', encoding, **kwargs)
+
+    def appendb(
+            self,
+            file_name: str,
+            file_contents: bytes,
+            **kwargs
+    ):
+        """
+        Save the contents to a file.
+
+        Parameters
+        ----------
+        file_name : str
+            The name of the file to save.
+        file_contents : str
+            The contents to save to the file.
+        encoding : str, optional
+            The encoding of the file, by default "utf-8".
+        **kwargs
+            Additional keyword arguments to pass to the open() function.
+
+        """
+        self._open_write(file_name, file_contents, 'ab', encoding=None, **kwargs)
+
+    def read(self, file_name, encoding="utf-8", **kwargs):
+        """
+        Load the contents of a file.
+
+        Parameters
+        ----------
+        file_name : str
+            The name of the file to load.
+        encoding : str, optional
+            The encoding of the file, by default "utf-8".
+        **kwargs
+            Additional keyword arguments to pass to the open() function.
+
+        Returns
+        -------
+        str or None
+            The contents of the file, or None if the file was not found.
+
+        """
+        return self._open_read(file_name, encoding=encoding, **kwargs)
+
+    def readb(self, file_name: str, **kwargs):
+        """
+        Load the contents of a file.
+
+        Parameters
+        ----------
+        file_name : str
+            The name of the file to load.
+        encoding : str, optional
+            The encoding of the file, by default "utf-8".
+        **kwargs
+            Additional keyword arguments to pass to the open() function.
+
+        Returns
+        -------
+        str or None
+            The contents of the file, or None if the file was not found.
+
+        """
+        return self._open_read(file_name, mode='rb', encoding=None, **kwargs)
+
+
+class Compressor:
+    def __init__(self,
+                 filename: str,
+                 wdir: str = '',
+                 extension: str = 'zip') -> None:
+        self.extension = extension
+        self.filename = f"{filename}.{self.extension}"
+        self.manager = FilesManager(wdir)
+        self.file_path = self.manager.folder_path / self.filename
+        self.members = {}
+        if self.file_path.name not in self.manager.list_files():
+            with zipfile.ZipFile(self.file_path, mode="w") as _:
+                pass
+
+    def path(self,):
+        return self.manager.folder_path / self.file_path
+
+    def namelist(self,):
+        if self.file_path.name in self.manager.list_files():
+            with zipfile.ZipFile(self.file_path, mode="r") as archive:
+                return [Path(name).name for name in archive.namelist()]
+        return []
+
+    def add(self, filename: str, delete_source=True, mode='a') -> None:
+        if filename in self.manager.list_files():
+            with zipfile.ZipFile(self.file_path, mode=mode) as archive:
+                archive.write(self.manager.file_path(filename), filename)
+                if delete_source:
+                    self.manager.delete_file(self.manager.file_path(filename))
+                self.members[filename] = archive.namelist()[-1]
+        else:
+            logger.warning(
+                'File %s not found in working directory %s',
+                filename,
+                self.manager.folder_path
+            )
+
+    def write(self, filename: str, content: str, delete_source=True, mode='a') -> None:
+        self.manager.write(filename, content)
+        self.add(filename, delete_source=delete_source, mode=mode)
+
+    def writeb(self, filename: str, content: bytes, delete_source=True, mode='a') -> None:
+        self.manager.writeb(filename, content)
+        self.add(filename, delete_source=delete_source, mode=mode)
+
+    def append(self, filename: str, content: str) -> None:
+        data = self.read(filename)
+        if filename in self.manager.list_files():
+            with zipfile.ZipFile(self.file_path, mode="w") as archive:
+                with archive.open(self.members[filename], mode='w') as member:
+                    data += content
+                    member.write(data.encode('utf-8'))
+        else:
+            logger.warning(
+                'File %s not found in working directory %s',
+                filename,
+                self.manager.folder_path
+            )
+
+    def appendb(self, filename: str, content: bytes) -> None:
+        data = self.read(filename, as_text=False)
+        if filename in self.manager.list_files():
+            with zipfile.ZipFile(self.file_path, mode="w") as archive:
+                with archive.open(self.members[filename], mode='w') as member:
+                    data += content
+                    member.write(data)
+        else:
+            logger.warning(
+                'File %s not found in working directory %s',
+                filename,
+                self.manager.folder_path
+            )
+
+    def read(self, filename: str, as_text=True) -> str | bytes:
+        with zipfile.ZipFile(self.file_path, mode="r") as archive:
+            with archive.open(self.members[filename], mode='r') as member:
+                data = member.read()
+                if as_text:
+                    return data.decode('utf-8')
+                return data
+
+    def extract(self, filename: str) -> str:
+        with zipfile.ZipFile(self.file_path, mode="r") as archive:
+            return archive.extract(
+                self.members[filename],
+                path=self.manager.folder_path
+            )
--- tests/test_centopy.py
+++ tests/test_centopy.py
@@ -2,15 +2,19 @@ import os
 import shutil
 import unittest
 import tempfile
+import zipfile
+
+from unittest.mock import patch
 
 from centopy.base import BaseFilesManager
 from centopy.core import FilesManager
+from centopy.core import Compressor
 
 
 class TestBaseFilesManager(unittest.TestCase):
     def setUp(self):
         self.temp_dir = tempfile.mkdtemp()
-        self.file_manager = BaseFilesManager(self.temp_dir)
+        self.manager = BaseFilesManager(self.temp_dir)
     
     def tearDown(self):
         shutil.rmtree(self.temp_dir)
@@ -18,35 +22,35 @@ class TestBaseFilesManager(unittest.TestCase):
     def test_file_creation(self):
         # Test creating a new file
         file_name = "test_file.txt"
-        file_path = self.file_manager.file_path(file_name)
+        file_path = self.manager.file_path(file_name)
         self.assertFalse(file_path.exists())
         with open(file_path, "w", encoding='utf-8') as f:
             f.write("Test content")
         self.assertTrue(file_path.exists())
         self.assertEqual(
-            self.file_manager.get_file_state(file_name), "unknown"
+            self.manager.get_file_state(file_name), "unknown"
         )
 
     def test_list_files(self):
         # Test listing files in the directory
         files = ["file1.txt", "file2.txt", "file3.txt"]
         for file_name in files:
-            file_path = self.file_manager.file_path(file_name)
+            file_path = self.manager.file_path(file_name)
             with open(file_path, "w", encoding='utf-8') as f:
                 f.write("Test content")
-        listed_files = self.file_manager.list_files()
+        listed_files = self.manager.list_files()
         self.assertEqual(set(files), set(listed_files))
 
     def test_delete_file(self):
         # Test deleting a file
         file_name = "test_file.txt"
-        file_path = self.file_manager.file_path(file_name)
+        file_path = self.manager.file_path(file_name)
         with open(file_path, "w", encoding='utf-8') as f:
             f.write("Test content")
         self.assertTrue(file_path.exists())
-        self.file_manager.delete_file(file_name)
+        self.manager.delete_file(file_name)
         self.assertFalse(file_path.exists())
-        self.assertEqual(self.file_manager.get_file_state(file_name), "deleted")
+        self.assertEqual(self.manager.get_file_state(file_name), "deleted")
 
     def test_invalid_folder_path(self):
         # Test invalid folder path raises ValueError
@@ -61,50 +65,95 @@ class TestBaseFilesManager(unittest.TestCase):
             "Nothing to delete"
         )
         with self.assertLogs('standard', level="WARNING") as cm:
-            self.file_manager.delete_file(file_name)
+            self.manager.delete_file(file_name)
             print(cm.output)
         self.assertIn(expected_log_message, cm.output)
 
     def test_str_representation(self):
         # Test __str__ representation of the object
         file_name = "test_file.txt"
-        file_path = self.file_manager.file_path(file_name)
+        file_path = self.manager.file_path(file_name)
         with open(file_path, "w", encoding='utf-8') as f:
             f.write("Test content")
         expected_str = f"Working dir: {self.temp_dir}\n{file_name}: unknown"
-        self.assertEqual(str(self.file_manager), expected_str)
+        self.assertEqual(str(self.manager), expected_str)
 
 class TestFilesManager(unittest.TestCase):
     def setUp(self):
         self.temp_dir = tempfile.mkdtemp()
-        self.file_manager = FilesManager(self.temp_dir)
+        self.manager = FilesManager(self.temp_dir)
 
     def tearDown(self):
         shutil.rmtree(self.temp_dir)
 
-    def test_save_and_load_file(self):
-        # Test saving and loading a file
-        file_name = "test_file.txt"
-        file_contents = "Test content"
-        self.file_manager.write(file_name, file_contents)
-        loaded_contents = self.file_manager.read(file_name)
-        self.assertEqual(loaded_contents, file_contents)
-        self.assertEqual(self.file_manager.get_file_state(file_name), "loaded")
+    def test_write_and_read(self):
+        file_name = 'test_write.txt'
+        content = "Test content"
+        
+        self.manager.write(file_name, content)
+        read_content = self.manager.read(file_name)
+        
+        self.assertEqual(read_content, content)
 
-    def test_load_nonexistent_file(self):
+    def test_writeb_and_readb(self):
+        file_name = 'test_writeb.txt'
+        content = b'Test content'
+        
+        self.manager.writeb(file_name, content)
+        read_content = self.manager.readb(file_name)
+        
+        self.assertEqual(read_content, content)
+
+    def test_append_and_read(self):
+        file_name = 'test_append.txt'
+        content1 = "First line\n"
+        content2 = "Second line\n"
+        
+        self.manager.write(file_name, content1)
+        self.manager.append(file_name, content2)
+        
+        read_content = self.manager.read(file_name)
+        
+        expected_content = content1 + content2
+        self.assertEqual(read_content, expected_content)
+
+    def test_appendb_and_readb(self):
+        file_name = 'test_appendb.txt'
+        content1 = b'First line\n'
+        content2 = b'Second line\n'
+        
+        self.manager.writeb(file_name, content1)
+        self.manager.appendb(file_name, content2)
+        
+        read_content = self.manager.readb(file_name)
+        
+        expected_content = content1 + content2
+        self.assertEqual(read_content, expected_content)
+
+    def test_delete_file(self):
+        file_name = 'test_delete.txt'
+        content = "Test content"
+        
+        self.manager.write(file_name, content)
+        self.manager.delete_file(file_name)
+        
+        self.assertFalse(os.path.exists(self.manager.file_path(file_name)))
+
+    def test_get_file_state(self):
+        file_name = 'test_state.txt'
+        content = "Test content"
+        
+        self.manager.write(file_name, content)
+        state = self.manager.get_file_state(file_name)
+        
+        self.assertEqual(state, "saved")
+
+    def test_read_nonexistent_file(self):
         # Test loading a nonexistent file
         file_name = "nonexistent_file.txt"
-        loaded_contents = self.file_manager.read(file_name)
+        loaded_contents = self.manager.read(file_name)
         self.assertIsNone(loaded_contents)
-        self.assertEqual(self.file_manager.get_file_state(file_name), "failed")
-
-    def test_read_and_write_file(self):
-        # Test reading and writing a file
-        file_name = "test_file.txt"
-        file_contents = "Test content"
-        self.file_manager.write(file_name, file_contents)
-        read_contents = self.file_manager.read(file_name)
-        self.assertEqual(read_contents, file_contents)
+        self.assertEqual(self.manager.get_file_state(file_name), "failed")
 
     def test_invalid_folder_path(self):
         # Test invalid folder path raises ValueError
@@ -115,12 +164,103 @@ class TestFilesManager(unittest.TestCase):
         # Test listing files in the directory
         files = ["file1.txt", "file2.txt", "file3.txt"]
         for file_name in files:
-            file_path = self.file_manager.file_path(file_name)
+            file_path = self.manager.file_path(file_name)
             with open(file_path, "w", encoding='utf-8') as f:
                 f.write("Test content")
-        listed_files = self.file_manager.list_files()
+        listed_files = self.manager.list_files()
         self.assertEqual(set(files), set(listed_files))
 
+
+class TestCompressor(unittest.TestCase):
+
+    def setUp(self):
+        self.temp_dir =  tempfile.mkdtemp()
+        self.compressor = Compressor('test', wdir=self.temp_dir)
+
+    def tearDown(self):
+        shutil.rmtree(self.temp_dir)
+
+    def test_init(self):
+        self.assertTrue(self.compressor.path().exists())
+
+    def test_namelist_non_existing_file(self):
+        names = self.compressor.namelist()
+        self.assertEqual(names, [])
+
+    def test_namelist_existing_file(self):
+        file_name = "file.txt"
+        file_path = self.compressor.manager.file_path(file_name)
+        self.assertFalse(file_path.exists())
+        with open(file_path, 'w', encoding='utf-8') as f:
+            f.write('Test data')
+        self.compressor.add(file_name, delete_source=False)
+        names = self.compressor.namelist()
+        self.assertIn(file_name, names)
+        self.assertIn(file_name, self.compressor.manager.list_files())
+
+    def test_add_non_existing_file(self):
+        with patch('centopy.core.logger') as mock_logger:
+            self.compressor.add('non_existent_file.txt')
+            mock_logger.warning.assert_called_once()
+
+    def test_write_and_read_text_file(self):
+        self.compressor.write('test.txt', 'Hello, World!')
+        content = self.compressor.read('test.txt')
+        self.assertEqual(content, 'Hello, World!')
+
+    def test_write_and_read_binary_file(self):
+        content = b'Test binary data'
+        file_name = "test.bin"
+        file_path = self.compressor.manager.file_path(file_name)
+        with open(file_path, 'wb') as bin_file:
+            bin_file.write(content)
+        self.compressor.add(file_name, delete_source=False)
+        read_content = self.compressor.read('test.bin', as_text=False)
+        self.assertEqual(read_content, content)
+
+    def test_extract_file(self):
+        content = 'Extract me'
+        file_name = 'test.txt'
+        file_path = self.compressor.manager.file_path(file_name)
+        self.compressor.write(file_name, content)
+        extracted_path = self.compressor.extract(file_name)
+        self.assertEqual(extracted_path, str(file_path))
+        with open(file_path, 'r', encoding='utf-8') as file_:
+            extracted_content = file_.read()
+            self.assertEqual(extracted_content, 'Extract me')
+    
+    def test_writeb_and_readb(self):
+        file_name = 'test_writeb.txt'
+        content = b'Test binary content'
+        
+        self.compressor.writeb(file_name, content)
+                
+        read_content = self.compressor.read(file_name, as_text=False)
+        self.assertEqual(read_content, content)
+
+    def test_append(self):
+        file_name = 'test_append.txt'
+        content1 = 'First binary line\n'
+        content2 = 'Second binary line\n'
+        
+        self.compressor.write(file_name, content1, delete_source=False)
+        self.compressor.append(file_name, content2)
+                
+        read_content = self.compressor.read(file_name, as_text=True)
+        expected_content = content1 + content2
+        self.assertEqual(read_content, expected_content)
+
+    def test_appendb(self):
+        file_name = 'test_appendb.txt'
+        content1 = b'First binary line\n'
+        content2 = b'Second binary line\n'
+        
+        self.compressor.writeb(file_name, content1, delete_source=False)
+        self.compressor.appendb(file_name, content2)
+                
+        read_content = self.compressor.read(file_name, as_text=False)
+        expected_content = content1 + content2
+        self.assertEqual(read_content, expected_content)
 
 if __name__ == "__main__":
     unittest.main()
